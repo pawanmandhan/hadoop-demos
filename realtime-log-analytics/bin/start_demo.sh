9393fb622fc7f048c382f3b0979f85a8fed9ad54
root <root@sandbox.hortonworks.com>
2015-01-22 11:35:09 +0000

    flume hdfs sink working

---
+++ realtime-log-analytics/bin/start_demo.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+echo "Run the following command - /root/demo/hadoop-demos/realtime-log-analytics/log-generator/generate_logs.py -f events.log"
+
+echo " TO start the the flume agent use ths command - flume-ng agent -c /etc/flume/conf -f /etc/flume/conf/flume.conf -n sandbox"

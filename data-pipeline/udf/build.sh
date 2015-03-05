126d979f2cb7f9b4e5704b84f0a5f7c3e42f6af9
root <root@sandbox.hortonworks.com>
2015-03-05 12:47:38 +0000

    updated build.sh

--- data-pipeline/udf/build.sh
+++ data-pipeline/udf/build.sh
@@ -1,1 +1,1 @@
- javac -classpath /usr/hdp/2.2.0.0-2041/hadoop/client/hadoop-common-2.6.0.2.2.0.0-2041.jar XML2JSONConvertor.java 
+ javac -classpath ./:/usr/hdp/2.2.0.0-2041/hadoop/client/hadoop-common-2.6.0.2.2.0.0-2041.jar XML2JSONConvertor.java 

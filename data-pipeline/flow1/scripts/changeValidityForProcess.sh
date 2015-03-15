d372cddcd5d56c76b5f1522d60a4319993252a46
root <root@sandbox.hortonworks.com>
2015-03-15 15:12:20 +0000

    scripting

---
+++ data-pipeline/flow1/scripts/changeValidityForProcess.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set +vx
+service ntpd start
+chkconfig ntpd on
+
+project_root="/app/data-pipeline/flow1"
+
+echo "Changing Process File"
+cd ${project_root}/falcon/process
+cp processData.xml processData.xml.ORIGINAL
+
+vl=`grep -n "validity" processData.xml.ORIGINAL | awk -F':' '{ print $1 }'`
+vlb=$(( $vl - 1 ))
+vla=$(( $vl + 1 ))
+
+ve=$(date -d "+10 days" +"%Y-%m-%dT%H:%MZ")
+vs=$(date -d "+5 minutes" +"%Y-%m-%dT%H:%MZ")
+
+head -n $vlb processData.xml.ORIGINAL  >> processData.xml.new
+echo "<validity start=\"${vs}\" end=\"${ve}\" />" >> processData.xml.new
+tail -n $vla  processData.xml.ORIGINAL >> processData.xml.new
+
+rm processData.xml
+mv processData.xml.new processData.xml
--- data-pipeline/flow1/scripts/copyAppToHDFS.sh
+++ data-pipeline/flow1/scripts/copyAppToHDFS.sh
@@ -2,12 +2,17 @@
 
 
 un=`whoami`
+project_root="/app/data-pipeline/flow1"
 if [ $un == 'root' ]; then
 
-su - ambari-qa -c "hdfs dfs -put ../hql/* /user/ambari-qa/data_pipeline_demo/hql/"
-su - ambari-qa -c "hdfs dfs -put ../pig/* /user/ambari-qa/data_pipeline_demo/pig/"
-su - ambari-qa -c "hdfs dfs -put ../falcon/workflow/* /user/ambari-qa/data_pipeline_demo/falcon/workflow/*"
+su - ambari-qa -c "hdfs dfs -put ${project_root}/hql/* /user/ambari-qa/data_pipeline_demo/hql/"
+echo "Copied HQL files to HDFS"
+su - ambari-qa -c "hdfs dfs -put ${project_root}/pig/* /user/ambari-qa/data_pipeline_demo/pig/"
+echo "Copied pig files to HDFS"
+su - ambari-qa -c "hdfs dfs -put ${project_root}/falcon/workflow/* /user/ambari-qa/data_pipeline_demo/falcon/workflow/"
+echo "Copied Falcon workflow files to HDFS"
 su - ambari-qa -c "hdfs dfs -put /etc/hive/conf/hive-site.xml /user/ambari-qa/data_pipeline_demo/conf"
+echo "Copied conf files to HDFS"
 
 else
 

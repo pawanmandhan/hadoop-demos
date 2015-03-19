becfc350e1513e8621026807683c6f0d218f6df1
root <root@sandbox.hortonworks.com>
2015-03-19 23:38:43 +0000

    reset script

--- data-pipeline/flow1/scripts/resetDemo.sh
+++ data-pipeline/flow1/scripts/resetDemo.sh
@@ -10,7 +10,12 @@ su - ambari-qa -c "hdfs dfs -rm -r /user/ambari-qa/data_pipeline_demo/data/input
 echo "Cleaned up data directory"
 
 echo "Cleaning up hive table"
-su - ambari-qa -c "hdfs dfs -rm -r /user/ambari-qa/data_pipeline_demo/data/input/*"
+su - ambari-qa -c "hdfs dfs -rm -r /user/ambari-qa/data_pipeline_demo/hivedb/raw_xml/*"
+su - ambari-qa -c "hdfs dfs -rm -r /user/ambari-qa/data_pipeline_demo/hivedb/raw_json/*"
+su - ambari-qa -c "hdfs dfs -rm -r /user/ambari-qa/data_pipeline_demo/hivedb/sv_aggregate/*"
+su - ambari-qa -c "hdfs dfs -rm -r /user/ambari-qa/data_pipeline_demo/hivedb/sv_json_data/*"
+su - ambari-qa -c "hdfs dfs -rm -r /user/ambari-qa/data_pipeline_demo/hivedb/sv_json_data_master/*"
+su - ambari-qa -c "hdfs dfs -rm -r /user/ambari-qa/data_pipeline_demo/hivedb/xml_data_archive/*"
 
 
 echo "Cleaning up mysql table"

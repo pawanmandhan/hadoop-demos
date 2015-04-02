05b8035140637b87a2602ae6ba7282e22a2ce656
root <root@sandbox.hortonworks.com>
2015-04-02 02:44:43 +0000

    added flume scripts

---
+++ data-pipeline/flow1/scripts/runTest.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+rm -rf /root/data_pipeline_demo/input/SV-sample*xml
+cp /app/hadoop-demos/data-pipeline/flow1/scripts/SV-sample-1.xml /root/data_pipeline_demo/input/
+cp /app/hadoop-demos/data-pipeline/flow1/scripts/SV-sample-2.xml /root/data_pipeline_demo/input/
+cp /app/hadoop-demos/data-pipeline/flow1/scripts/SV-sample-3.xml /root/data_pipeline_demo/input/
+
